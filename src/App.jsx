7aa915d112016edd0a18df426ca5782f44a0852f
SamueleTerraroli <terraroli99@gmail.com>
2025-02-01 15:37:27 +0100

    feat: add display error

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
+import PostsPage from "./pages/PostsPage"
+import NotFound from "./pages/NotFound"
+
 import { GlobalProvider } from "./context/GlobalContext"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
@@ -6,6 +11,11 @@ const App = () => {
     <GlobalProvider>
       <BrowserRouter>
         <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={HomePage} />
+            <Route path="posts" Component={PostsPage} />
+            <Route path="*" Component={NotFound} />
+          </Route>
 
         </Routes>
       </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+const NotFound = () => {
+    return (
+        <div>NotFound404</div>
+    )
+}
+
+export default NotFound
